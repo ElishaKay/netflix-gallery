548d740433b3d531a37e1cf48848cdc21a512e9a
gaspar belandria <fdirinot@hotmail.com>
2016-02-28 23:36:07 -0500

    added opacity dynamic on scroller arrows

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,6 @@
 angular.module('galleryApp', [])
     .controller('GalleryController', function($scope) {
+
         $scope.gallery = [{id:'1'},{id:'2'},{id:'3'},{id:'4'},{id:'5'},{id:'6'},{id:'7'},{id:'8'},{id:'9'},{id:'10'},{id:'11'},{id:'12'},{id:'13'},{id:'14'},{id:'15'},{id:'16'},{id:'17'},{id:'18'},{id:'19'},{id:'20'},{id:'21'},{id:'22'},{id:'23'},{id:'24'},{id:'25'}];
 
     }).directive('galleryScroll', [function() {
@@ -8,19 +9,46 @@ angular.module('galleryApp', [])
             link: function(scope, elem, attrs) {
                 var scrollToLeft = function(e){
                     var moveTo = items[0].getBoundingClientRect().left + 300 + 'px';
-                    items[0].style.transition = 'all 1s ease-out';
+                    items[0].style.transition = 'all 300ms ease-out';
                     items[0].style.transform = 'translateX('+moveTo+')';
                 };
                 var scrollToRight = function(e){
                     var moveTo = items[0].getBoundingClientRect().left - 300 + 'px';
-                    items[0].style.transition = 'all 1s ease-out';
+                    items[0].style.transition = 'all 300ms ease-out';
                     items[0].style.transform = 'translateX('+moveTo+')';
                 };
+                var opacityScrollers = function(event){
+                    var mymodelsArrowRightTop = (document.querySelector('.right').getBoundingClientRect().height/2) + document.querySelector('.right').getBoundingClientRect().top,
+                        mymodelsArrowRightMiddle = (document.querySelector('.right').getBoundingClientRect().width/2) + document.querySelector('.right').getBoundingClientRect().left,
+                        mathForRight = Math.round(Math.sqrt(Math.pow(mymodelsArrowRightTop - event.clientY, 2) +Math.pow(mymodelsArrowRightMiddle - event.clientX, 2))),
+                        rightOpacity = 1-(mathForRight/(window.innerHeight));
+                    document.getElementsByClassName('right')[0].style.opacity = rightOpacity;
+                    //
+                    var mymodelsArrowLeftTop = (document.querySelector('.left').getBoundingClientRect().height/2) + document.querySelector('.left').getBoundingClientRect().top,
+                        mymodelsArrowLeftMiddle = (document.querySelector('.left').getBoundingClientRect().width/2) + document.querySelector('.left').getBoundingClientRect().left,
+                        mathForLeft = Math.round(Math.sqrt(Math.pow(mymodelsArrowLeftTop - event.clientY, 2) +Math.pow(mymodelsArrowLeftMiddle - event.clientX, 2))),
+                        leftOpacity = 1-(mathForLeft/(window.innerHeight));
+                    document.getElementsByClassName('left')[0].style.opacity = leftOpacity;
+
+                    console.log(document.getElementsByClassName('left')[0]);
+                };
+                var doc = angular.element(document);
                 var items = angular.element(document.querySelector('.items'));
                 var right = angular.element(document.querySelector('.right'));
                 var left = angular.element(document.querySelector('.left'));
                 right.bind('click', scrollToRight);
                 left.bind('click', scrollToLeft);
+                doc.bind('mouseover', opacityScrollers);
+
+
+
+
+                elem.on('$destroy', function () {
+                    right.unbind('click', scrollToRight);
+                    left.unbind('click', scrollToLeft);
+                    items = right = left = null;
+                });
+
             }
         }
     }]);
